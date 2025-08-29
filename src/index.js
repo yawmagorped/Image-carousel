f7223ff521b36b597d09d8f3b026b514afce889c
ali <193356044+yawmagorped@users.noreply.github.com>
2025-08-29 23:26:19 +0330

    add timer functionality

--- src/index.js
+++ src/index.js
@@ -1,96 +1,104 @@
 import "./style.css";
 
-let b = require.context('./images/', true, /\.(jpg|jpeg)$/);
+let b = require.context("./images/", true, /\.(jpg|jpeg)$/);
 let imageSources = b.keys().map(b);
 
-let emptyDot = require('./images/dot-svgrepo-com.svg');
-let filledDot = require('./images/dot-fill-svgrepo-com.svg');
-const handleCarousel = () => {
-    const container = document.querySelector(".container");
-    const prevImg = container.querySelector("#previous-img > img");
-    const currImg = container.querySelector("#current-img > img");
-    const nextImg = container.querySelector("#next-img > img");
-    
-    let currIndex = 0;
+let emptyDot = require("./images/dot-svgrepo-com.svg");
+let filledDot = require("./images/dot-fill-svgrepo-com.svg");
+const handleCarousel = (() => {
+  const container = document.querySelector(".container");
+  const prevImg = container.querySelector("#previous-img > img");
+  const currImg = container.querySelector("#current-img > img");
+  const nextImg = container.querySelector("#next-img > img");
 
-    const createImageDots = (i) => {
-        let img = document.createElement("img");
-        img.src = emptyDot;
-        img.id = `dot${i}`;
-        return img;
+  let currIndex = 0;
+  let timer;
+
+  const createImageDots = (i) => {
+    let img = document.createElement("img");
+    img.src = emptyDot;
+    img.id = `dot${i}`;
+    return img;
+  };
+
+  const dotsContainer = document.querySelector(".dots");
+
+  for (let index = 0; index < imageSources.length; index++) {
+    let dot = createImageDots(index);
+    dotsContainer.append(dot);
+    dot.addEventListener("click", () => {
+      currIndex = index;
+      emptyDots();
+      setCarouselFocus(index);
+    });
+  }
+
+  const leftArrow = document.querySelector("#left-arrow");
+  const rightArrow = document.querySelector("#right-arrow");
+
+  const emptyDots = () => {
+    let images = document.querySelectorAll(".dots img");
+    images.forEach((image) => {
+      image.src = emptyDot;
+    });
+  };
+
+  const setImageSources = (prev, curr, next) => {
+    prevImg.src = prev;
+    currImg.src = curr;
+    nextImg.src = next;
+  };
+
+  const setCarouselFocus = (i) => {
+    resetTimer();
+    if (i == 0)
+      setImageSources(
+        imageSources[imageSources.length - 1],
+        imageSources[i],
+        imageSources[i + 1],
+      );
+    else if (i == imageSources.length - 1)
+      setImageSources(imageSources[i - 1], imageSources[i], imageSources[0]);
+    else
+      setImageSources(
+        imageSources[i - 1],
+        imageSources[i],
+        imageSources[i + 1],
+      );
+    let dot = document.querySelector(`#dot${i}`);
+    dot.src = filledDot;
+  };
+
+  const rotateCarousel = (dirString, clankerMode) => {
+    if (clankerMode != true) {
+      resetTimer();
     }
 
-    const dotsContainer = document.querySelector(".dots");
+    emptyDots();
+    if (dirString == "left") {
+      if (currIndex == 0) currIndex = imageSources.length - 1;
+      else currIndex--;
 
-    for (let index = 0; index < imageSources.length; index++) {
-        let dot = createImageDots(index);
-        dotsContainer.append(dot);
-        dot.addEventListener('click', (e) => {
-            currIndex = index;
-            emptyDots();
-            setCarouselFocus(index);
-        })
+      setCarouselFocus(currIndex);
+    } else if (dirString == "right") {
+      if (currIndex == imageSources.length - 1) currIndex = 0;
+      else currIndex++;
+
+      setCarouselFocus(currIndex);
     }
-    
-    const leftArrow = document.querySelector("#left-arrow");
-    const rightArrow = document.querySelector("#right-arrow");
-    
-    
-    
-    const emptyDots = () => {
-        let images = document.querySelectorAll(".dots img");
-        images.forEach(image => {
-            image.src = emptyDot;
-        });
+  };
+
+  leftArrow.addEventListener("click", () => rotateCarousel("left", false));
+  rightArrow.addEventListener("click", () => rotateCarousel("right", false));
+
+  const resetTimer = () => {
+    if (timer) {
+      clearInterval(timer);
+      timer = setInterval(rotateCarousel, 5000, "right", true);
     }
+  };
 
-    const setImageSources = (prev, curr, next) => {
-        prevImg.src = prev;
-        currImg.src = curr;
-        nextImg.src = next;
-    } 
-    
-    const setCarouselFocus = (i) => {
-        if (i == 0)
-            setImageSources(imageSources[imageSources.length - 1], imageSources[i], imageSources[i+1]);
-        else if (i == (imageSources.length - 1)) 
-            setImageSources(imageSources[i - 1], imageSources [i], imageSources[0]);
-        else
-            setImageSources(imageSources[i-1], imageSources[i], imageSources[i+1]);
-        let dot = document.querySelector(`#dot${i}`)
-        dot.src = filledDot;
-    }
+  timer = setInterval(rotateCarousel, 5000, "right", true);
 
-    const rotateCarousel = (dirString) => {
-        emptyDots();
-        if (dirString == "left") {
-            if(currIndex == 0)
-                currIndex = imageSources.length - 1;
-            else
-                currIndex--;
-            
-            setCarouselFocus(currIndex);
-        } else if (dirString == "right") {
-            if(currIndex == imageSources.length - 1)
-                currIndex = 0;
-            else
-                currIndex++;
-            
-            setCarouselFocus(currIndex)
-        }
-    }
-
-    leftArrow.addEventListener('click', () => rotateCarousel("left"));
-    rightArrow.addEventListener('click', () => rotateCarousel("right"));
-    
-    
-    setCarouselFocus(currIndex);
-
-    //let timer = setInterval(autoProgress, 5000);
-
-    //const stopTimer = () => {};
-
-    //const autoProgress = () => {};
-};
-
-handleCarousel();
+  setCarouselFocus(currIndex);
+})();
